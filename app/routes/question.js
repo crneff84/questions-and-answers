80618ef194d47549c5efb7b0bc252e5f5965c7bd
Chance Neff <crneff84@gmail.com>
2016-10-28 10:29:48 -0700

    add individual pages for questions and delete question functionality

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    destroyQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(comment){
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function(){
+        return question.destroyRecord();
+      });
+      this.transitionTo('index')
+    },
+
+  }
 });

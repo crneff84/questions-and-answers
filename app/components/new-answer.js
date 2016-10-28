77e35f3519bfccb7f1e369380295bc9b50ab63dd
Chance Neff <crneff84@gmail.com>
2016-10-28 11:09:53 -0700

    add ability to add answers to questions

---
+++ app/components/new-answer.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShown(){
+      this.set("addNewAnswer", true);
+    },
+    saveAnswer(){
+      var params={
+        answer: this.get('answer'),
+        author: this.get('author'),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
+});

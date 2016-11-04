86dbe57c6dc45606e301d1f2d5c66731d6638f42
Chance Neff <crneff84@gmail.com>
2016-11-04 09:31:21 -0700

    add basic functionality for ember service to add and remove questions from a favorites list

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favorite: Ember.inject.service(),
+
   answercount: Ember.computed('question.answers', function() {
     var count = this.get('question.answers.content.length');
     return count;
   }),
   actions: {
+    addToFavorites(question) {
+      this.get('favorite').add(question);
+    },
+    removeFromFavorites(question) {
+      this.get('favorite').remove(question);
+    },
     destroyQuestion(question) {
       if(confirm("Are you sure you want to delete this question?")){
         this.sendAction('destroyQuestion', question);
--- app/services/favorite.js
+++ app/services/favorite.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  favoriteQuestions: [],
+
+  add(question) {
+    this.get('favoriteQuestions').pushObject(question);
+  },
+  remove(question) {
+    this.get('favoriteQuestions').removeObject(question);
+  }
+
 });

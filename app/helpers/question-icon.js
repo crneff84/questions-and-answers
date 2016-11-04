cb7d7330f41c2d36fa967fede20a220b5f74e94b
Chance Neff <crneff84@gmail.com>
2016-11-04 12:09:29 -0700

    adjust styling

--- app/helpers/question-icon.js
+++ app/helpers/question-icon.js
@@ -6,6 +6,9 @@ export function questionIcon(params) {
   if (question.get('answers').get('length') >= 2) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-education"</span>');
   }
+  if (question.get('answers').get('length') < 1) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-alert"</span>');
+  }
 }
 
 export default Ember.Helper.helper(questionIcon);
